230e84db12f59ddbd5ecaa54298a7a4f608da151
Gianghaha <giangluong229@gmai.com>
2025-03-02 22:17:45 +0700

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+#define fi first
+#define se second
+#define pii pair<ll, ll>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    // freopen("in.inp", "r", stdin);
+    // freopen("ou.out", "w", stdout);
+
+    cout << "hello";
+
+    return 0;
+}
